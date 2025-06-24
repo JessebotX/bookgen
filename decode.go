f81858b89bf6f8a7196e86a9f9b832d9db91d797
Jesse <jessebot.git@gmail.com>
2025-06-23 21:58:15 -0700

    refactor: remove unused setField function

--- decode.go
+++ decode.go
@@ -8,33 +8,8 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
-func setField(obj any, name string, value any) error {
-
-	structValue := reflect.ValueOf(obj).Elem()
-	structFieldValue := structValue.FieldByNameFunc(func (n string) bool {
-		return strings.EqualFold(n, name)
-	})
-
-	if !structFieldValue.IsValid() {
-		return nil
-	}
-
-	if !structFieldValue.CanSet() {
-		return fmt.Errorf("cannot set a value for field '%s'", name)
-	}
-
-	fieldType := structFieldValue.Type()
-	val := reflect.ValueOf(value)
-	if fieldType != val.Type() {
-		return fmt.Errorf("cannot set field '%v' (%v) to value '%v' (%v) because of mismatch type.\nValue must be of type %v", name, fieldType, val, val.Type(), fieldType)
-	}
-
-	structFieldValue.Set(val)
-	return nil
-}
-
-// Decode a map m into struct s. Field/Key names are
-// case-insensitive. Credit: <https://stackoverflow.com/a/26746461>
+// Decode a map m into struct s. Field/Key names are supposed to be
+// case sensitive. Credit: <https://stackoverflow.com/a/26746461>
 func mapToStruct(s any, m map[string]any) error {
 	for fieldName, v := range m {
 		reflectField := reflect.ValueOf(s).Elem()
