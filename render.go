ecc7241afe3792463d4c78baaa3b165be3168178
Jesse <jessebot.git@gmail.com>
2025-06-25 23:33:32 -0700

    feat: support base template if available

--- render.go
+++ render.go
@@ -9,6 +9,7 @@ import (
 
 func RenderCollectionToWebsite(c *Collection, workingDir, outputDir string) error {
 	layoutsDir := filepath.Join(workingDir, "layouts")
+	baseTemplatePath := filepath.Join(layoutsDir, "_base.html")
 	collectionTemplatePath := filepath.Join(layoutsDir, "index.html")
 	bookTemplatePath := filepath.Join(layoutsDir, "_book.html")
 	chapterTemplatePath := filepath.Join(layoutsDir, "_chapter.html")
@@ -20,14 +21,32 @@ func RenderCollectionToWebsite(c *Collection, workingDir, outputDir string) erro
 	// ---
 	// Read templates
 	// ---
-	t, err := template.ParseFiles(
-		collectionTemplatePath,
-		bookTemplatePath,
-		chapterTemplatePath,
-	)
-	if err != nil {
-		return fmt.Errorf("failed to parse template. %w", err)
+	currentParsing := []string{collectionTemplatePath}
+
+	if _, err := os.Stat(baseTemplatePath); err == nil {
+		currentParsing = append(currentParsing, baseTemplatePath)
+	} else if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to stat base template `%v`. %w", baseTemplatePath, err)
 	}
+
+	collectionTemplate, err := template.ParseFiles(currentParsing...)
+	if err != nil {
+		return fmt.Errorf("failed to parse collection template. %w", err)
+	}
+
+	currentParsing[0] = bookTemplatePath
+	bookTemplate, err := template.ParseFiles(currentParsing...)
+	if err != nil {
+		return fmt.Errorf("failed to parse book template. %w", err)
+	}
+
+	currentParsing[0] = chapterTemplatePath
+	chapterTemplate, err := template.ParseFiles(currentParsing...)
+	if err != nil {
+		return fmt.Errorf("failed to parse chapter template. %w", err)
+	}
+
+	_, _ = bookTemplate, chapterTemplate
 
 	// ---
 	// Collection index
@@ -38,7 +57,7 @@ func RenderCollectionToWebsite(c *Collection, workingDir, outputDir string) erro
 	}
 	defer outIndex.Close()
 
-	if err := t.ExecuteTemplate(outIndex, "index.html", c); err != nil {
+	if err := collectionTemplate.ExecuteTemplate(outIndex, "index.html", c); err != nil {
 		return fmt.Errorf("failed to write collection index file. %w", err)
 	}
 
