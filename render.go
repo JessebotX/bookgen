dbf9aa259cd88260f5f59450187ee9e0cc0a34a4
Jesse <jessebot.git@gmail.com>
2025-07-01 14:57:58 -0700

    feat: minify svg and js

--- render.go
+++ render.go
@@ -5,12 +5,15 @@ import (
 	"html/template"
 	"os"
 	"path/filepath"
+	"regexp"
 	"slices"
 	"strings"
 
 	"github.com/tdewolff/minify/v2"
 	"github.com/tdewolff/minify/v2/css"
 	"github.com/tdewolff/minify/v2/html"
+	"github.com/tdewolff/minify/v2/js"
+	"github.com/tdewolff/minify/v2/svg"
 )
 
 const (
@@ -31,6 +34,8 @@ func RenderCollectionToWebsite(c *Collection, workingDir, outputDir string, enab
 		KeepQuotes:          true,
 	})
 	minifier.AddFunc("text/css", css.Minify)
+	minifier.AddFuncRegexp(regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"), js.Minify)
+	minifier.AddFunc("image/svg+xml", svg.Minify)
 
 	if err := os.MkdirAll(outputDir, DirPerms); err != nil {
 		return fmt.Errorf("failed to create output directory. %w", err)
