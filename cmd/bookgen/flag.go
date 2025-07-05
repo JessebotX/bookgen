560d8b01148d5e6d17b996df307f25fb646f56aa
Jesse <jessebot.git@gmail.com>
2025-07-05 14:00:01 -0700

    refactor: clean up flag parsing

---
+++ cmd/bookgen/flag.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func flagParse(args []string, opts any) ([]string, error) {
+	posArgs := make([]string, 0)
+
+	optsStructValue := reflect.ValueOf(opts).Elem()
+	optsStructType := reflect.TypeOf(opts).Elem()
+
+	for i := 1; i < len(args); i++ {
+		isSet := false
+
+	outOpts:
+		for j := 0; j < optsStructType.NumField(); j++ {
+			field := optsStructType.Field(j)
+			flag, ok := field.Tag.Lookup("long")
+			if !ok {
+				return []string{}, fmt.Errorf("field %v missing/empty tag name (required)", field.Name)
+			}
+
+			short, shortExists := field.Tag.Lookup("short")
+
+			// Tries to match the following:
+			//
+			// --flag, -f, --flag=value, -f=value
+			if strings.EqualFold(args[i], "--"+flag) || strings.EqualFold(args[i], "-"+short) {
+				fieldValue := optsStructValue.FieldByName(field.Name)
+				if !fieldValue.CanSet() {
+					return []string{}, fmt.Errorf("arg '%v': field %v cannot be set", args[i], field.Name)
+				}
+
+				switch fieldValue.Kind() {
+				case reflect.Bool:
+					fieldValue.SetBool(true)
+				case reflect.String:
+					if (i + 1) >= len(args) {
+						return []string{}, fmt.Errorf("arg '%v': missing value argument", args[i])
+					}
+
+					fieldValue.SetString(args[i+1])
+				case reflect.Int:
+					if (i + 1) >= len(args) {
+						return []string{}, fmt.Errorf("arg '%v': missing value argument", args[i])
+					}
+
+					intArg, err := strconv.Atoi(args[i+1])
+					if err != nil {
+						return []string{}, fmt.Errorf("arg '%v': %w", args[i], err)
+					}
+
+					fieldValue.SetInt(int64(intArg))
+					i++
+				default:
+					return []string{}, fmt.Errorf("arg '%v': unsupported field type %v", args[i], fieldValue.Type())
+				}
+				isSet = true
+				break outOpts
+			} else if strings.HasPrefix(args[i], "--"+flag+"=") || (shortExists && strings.HasPrefix(args[i], "-"+short+"=")) {
+				argSplit := strings.SplitN(args[i], "=", 2)
+				if len(argSplit) != 2 {
+					return []string{}, fmt.Errorf("arg '%v': missing value after '='", args[i])
+				}
+
+				fieldValue := optsStructValue.FieldByName(field.Name)
+				if !fieldValue.CanSet() {
+					return []string{}, fmt.Errorf("arg '%v': field %v cannot be set", args[i], field.Name)
+				}
+
+				switch fieldValue.Kind() {
+				case reflect.String:
+					fieldValue.SetString(argSplit[1])
+				case reflect.Int:
+					intArg, err := strconv.Atoi(argSplit[1])
+					if err != nil {
+						return []string{}, fmt.Errorf("arg '%v': %w", args[i], err)
+					}
+
+					fieldValue.SetInt(int64(intArg))
+				default:
+					return []string{}, fmt.Errorf("arg '%v': unsupported field type %v", args[i], fieldValue.Type())
+				}
+
+				isSet = true
+				break outOpts
+			}
+		}
+
+		if !isSet {
+			posArgs = append(posArgs, args[i])
+		}
+	}
+
+	return posArgs, nil
+}
+
+func flagPrintHelp(opts any) {
+	indentSize := 4
+	fmt.Println("USAGE")
+
+	for range indentSize {
+		fmt.Printf(" ")
+	}
+
+	fmt.Println("bookgen [command] [flags...]")
+
+	fmt.Println()
+
+	fmt.Println("FLAGS")
+
+	optsStructValue := reflect.ValueOf(opts).Elem()
+	optsStructType := reflect.TypeOf(opts).Elem()
+
+	for i := 0; i < optsStructType.NumField(); i++ {
+		field := optsStructType.Field(i)
+		long, ok := field.Tag.Lookup("long")
+		if !ok {
+			continue
+		}
+
+		for range indentSize {
+			fmt.Printf(" ")
+		}
+
+		short, ok := field.Tag.Lookup("short")
+		if ok {
+			fmt.Printf("-%v, ", short)
+		} else {
+			fmt.Printf("    ")
+		}
+
+		fieldKind := optsStructValue.FieldByName(field.Name).Kind()
+		switch fieldKind {
+		case reflect.Bool:
+			fmt.Printf("--%v\n", long)
+		default:
+			fmt.Printf("--%v <value>\n", long)
+		}
+
+		desc, ok := field.Tag.Lookup("desc")
+		if !ok {
+			continue
+		}
+
+		for range indentSize * 2 {
+			fmt.Printf(" ")
+		}
+
+		fmt.Printf("%v\n", desc)
+	}
+}
--- cmd/bookgen/main.go
+++ cmd/bookgen/main.go
@@ -5,33 +5,24 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"time"
 
 	"github.com/JessebotX/bookgen"
 )
 
-type FlagType int
-
-const (
-	FlagString FlagType = iota
-	FlagBool
-)
-
 var (
-	Version              = "0.9.0"
-	NoNonEssentialOutput = false
-	EnablePlainOutput    = false
-	ProgramName          = "bookgen"
+	Version           = "0.9.1"
+	EnablePlainOutput = false
 )
 
-type Flag struct {
-	Name        string
-	Type        FlagType
-	ShortName   string
-	Description string
-	Value       string
-	IsSet       bool
+type Opts struct {
+	Help                 bool   `long:"help" short:"h" desc:"print help/usage information"`
+	Version              bool   `long:"version" short:"v" desc:"print program version"`
+	InputDirectory       string `long:"input-directory" short:"i" desc:"directory containing source files with a bookgen.yml"`
+	OutputDirectory      string `long:"output-directory" short:"o" desc:"directory to output distributable files"`
+	PlainOutput          bool   `long:"plain" desc:"remove terminal escape codes from printing into stdout/stderr"`
+	NoNonEssentialOutput bool   `long:"no-non-essential-output" short:"q" desc:"prevent printing non-error messages into stdout/stderr"`
+	Minify               bool   `long:"minify" desc:"Minify output/distributable files"`
 }
 
 func main() {
@@ -39,72 +30,34 @@ func main() {
 	// Read CLI arguments
 	// ---
 	// Home-made cli argument parsing
-	ProgramName = os.Args[0]
+	var opts Opts
 
-	inputDirFlag := Flag{
-		Name:        "input-directory",
-		Type:        FlagString,
-		ShortName:   "i",
-		Description: "The working/input directory that contains a bookgen.yml file.",
-		Value:       "./",
-	}
-	outputDirFlag := Flag{
-		Name:        "output-directory",
-		Type:        FlagString,
-		ShortName:   "o",
-		Description: "The output directory where the distributable contents will be generated in.",
-		Value:       "./out",
-	}
-	plainOutputFlag := Flag{
-		Name:        "plain",
-		Type:        FlagBool,
-		ShortName:   "",
-		Description: "Strip text styling/terminal escape codes from terminal output.",
-		Value:       "false",
-	}
-	noNonEssentialOutputFlag := Flag{
-		Name:        "no-non-essential-output",
-		Type:        FlagBool,
-		ShortName:   "q",
-		Description: "Disable non-essential terminal output.",
-		Value:       "false",
-	}
-	minifyFlag := Flag{
-		Name:        "minify",
-		Type:        FlagBool,
-		ShortName:   "",
-		Description: "Enable minification of output files.",
-		Value:       "false",
-	}
-
-	positionalArgs, err := flagParse([]*Flag{
-		&inputDirFlag,
-		&outputDirFlag,
-		&plainOutputFlag,
-		&noNonEssentialOutputFlag,
-		&minifyFlag,
-	})
+	_, err := flagParse(os.Args, &opts)
 	if err != nil {
 		errorExit(1, err.Error())
 	}
 
-	if len(positionalArgs) > 0 && !inputDirFlag.IsSet {
-		inputDirFlag.Value = positionalArgs[0]
+	if opts.Help {
+		flagPrintHelp(&opts)
+
+		os.Exit(0)
 	}
 
-	EnablePlainOutput, _ = strconv.ParseBool(plainOutputFlag.Value)
-	NoNonEssentialOutput, _ = strconv.ParseBool(noNonEssentialOutputFlag.Value)
-	enableMinify, _ := strconv.ParseBool(minifyFlag.Value)
+	if opts.Version {
+		fmt.Printf("bookgen version %v %v/%v\n", Version, runtime.GOOS, runtime.GOARCH)
+		os.Exit(0)
+	}
 
-	// make default output dir generate relative to working dir if
-	// output dir is not provided
-	if !outputDirFlag.IsSet {
-		outputDirFlag.Value = filepath.Join(inputDirFlag.Value, "out")
+	EnablePlainOutput = opts.PlainOutput
+
+	// Default output directory is relative to the input directory.
+	if opts.OutputDirectory == "" {
+		opts.OutputDirectory = filepath.Join(opts.InputDirectory, "out")
 	}
 
 	// output dir cannot be == input dir
-	if filepath.Clean(inputDirFlag.Value) == filepath.Clean(outputDirFlag.Value) {
-		errorExit(1, "output directory cannot be equal to the working/input directory (`%s` and `%s` reference the same path).", inputDirFlag.Value, outputDirFlag.Value)
+	if filepath.Clean(opts.InputDirectory) == filepath.Clean(opts.OutputDirectory) {
+		errorExit(1, "output directory cannot be equal to the working/input directory (`%s` and `%s` are the same).", opts.InputDirectory, opts.OutputDirectory)
 	}
 
 	// ---
@@ -115,144 +68,32 @@ func main() {
 
 	decodeTimeStart := time.Now()
 
-	collection, err := bookgen.DecodeCollection(inputDirFlag.Value)
+	collection, err := bookgen.DecodeCollection(opts.InputDirectory)
 	if err != nil {
 		errorExit(1, err.Error())
 	}
 
 	decodeTimeElapsed := time.Since(decodeTimeStart)
 
-	if !NoNonEssentialOutput {
+	if !opts.NoNonEssentialOutput {
 		fmt.Printf("Decoded (%v)\n", decodeTimeElapsed)
 	}
 
 	renderTimeStart := time.Now()
 
-	if err := RenderCollectionToWebsite(&collection, inputDirFlag.Value, outputDirFlag.Value, enableMinify); err != nil {
+	if err := RenderCollectionToWebsite(&collection, opts.InputDirectory, opts.OutputDirectory, opts.Minify); err != nil {
 		errorExit(1, err.Error())
 	}
 
 	renderTimeElapsed := time.Since(renderTimeStart)
 
-	if !NoNonEssentialOutput {
+	if !opts.NoNonEssentialOutput {
 		fmt.Printf("Generated website (%v)\n", renderTimeElapsed)
 	}
 
 	totalTimeElapsed := time.Since(totalTimeStart)
-	if !NoNonEssentialOutput {
+	if !opts.NoNonEssentialOutput {
 		fmt.Printf(terminalPrintBold("Done")+" (%v)\n", totalTimeElapsed)
-	}
-}
-
-func flagParse(flags []*Flag) ([]string, error) {
-	positionalArgs := make([]string, 0)
-
-	for i := 1; i < len(os.Args); i++ {
-		arg := os.Args[i]
-
-		if arg == "--" {
-			break
-		}
-
-		if arg == "--help" || arg == "-h" || arg == "-?" {
-			printHelp(flags)
-
-			os.Exit(0)
-		}
-
-		// TODO: support help and version flags here.
-		if arg == "--version" || arg == "-v" || arg == "-V" {
-			fmt.Printf("%v version %v %v/%v\n", ProgramName, Version, runtime.GOOS, runtime.GOARCH)
-
-			os.Exit(0)
-		}
-
-		flagSet := false
-		for _, f := range flags {
-			if arg == "--"+f.Name || (f.ShortName != "" && arg == "-"+f.ShortName) {
-				if f.Type == FlagString {
-					if (i + 1) >= len(os.Args) {
-						return positionalArgs, fmt.Errorf("missing value for `%v`. See `%v --help for more information`", arg, ProgramName)
-					}
-
-					f.IsSet = true
-					flagSet = true
-					f.Value = os.Args[i+1]
-					i++
-					break
-				}
-
-				if f.Type == FlagBool {
-					f.IsSet = true
-					flagSet = true
-					f.Value = "true"
-					break
-				}
-			}
-		}
-
-		if !flagSet {
-			positionalArgs = append(positionalArgs, arg)
-		}
-	}
-
-	return positionalArgs, nil
-}
-
-func printHelp(flags []*Flag) {
-	indentSize := 4
-	fmt.Println("USAGE")
-
-	for range indentSize {
-		fmt.Printf(" ")
-	}
-
-	fmt.Printf("%v [FLAGS...] [/path/to/input-directory]\n\n", ProgramName)
-	fmt.Println("FLAGS")
-
-	// Help flag
-	for range indentSize {
-		fmt.Printf(" ")
-	}
-	fmt.Println("-h, -?, --help")
-	for range indentSize * 3 {
-		fmt.Printf(" ")
-	}
-	fmt.Println("Get help information on how to use this program.")
-
-	// Version flag
-	for range indentSize {
-		fmt.Printf(" ")
-	}
-	fmt.Println("-v, -V, --version")
-	for range indentSize * 3 {
-		fmt.Printf(" ")
-	}
-	fmt.Println("Get program version.")
-
-	// Other defined flags
-	for _, f := range flags {
-		for range indentSize {
-			fmt.Printf(" ")
-		}
-
-		if f.ShortName != "" {
-			fmt.Printf("-%v, --%v", f.ShortName, f.Name)
-		} else {
-			fmt.Printf("    --%v", f.Name)
-		}
-
-		if f.Type == FlagString {
-			fmt.Printf(" <string>")
-		}
-
-		fmt.Printf("\n")
-
-		for range indentSize * 3 {
-			fmt.Printf(" ")
-		}
-
-		fmt.Printf("%v (default: %v)\n", f.Description, f.Value)
 	}
 }
 
