4c3a13e721b8b5f4dad64a416467772ef713c697
Jesse <jessebot.git@gmail.com>
2025-09-21 19:52:19 -0700

    chore: bump version

--- cmd/pub/version_command.go
+++ cmd/pub/version_command.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	Version           = "0.14.4"
+	Version           = "0.14.5"
 	Homepage          = "https://github.com/JessebotX/pub"
 	LicenseIdentifier = "BSL-1.0"
 	LicenseLink       = "https://github.com/JessebotX/pub/blob/master/LICENSE.txt"
